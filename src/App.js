cd510ec8db38e4e86ca26ccb7d3f9ae9c1198f14
Mónica Mariana Muñoz Torres <128828587+MonicaMunoz357@users.noreply.github.com>
2025-06-17 21:11:44 -0600

    Añadiendo la Home page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Monica Mariana Muñoz Torres
+          Monica Mariana Munoz Torres
         </p>
         <a
           className="App-link"
